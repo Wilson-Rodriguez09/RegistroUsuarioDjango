8701364a233f046b49aadffcb5798ccc2069942f
wilson-rodriguez09 <samboniwilson09@gamil.com>
2024-08-16 11:45:32 -0500

    edicion 3

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Persona
+from .form import PersonaForm
 
 # Create your views here.
+
+def registerUser(request):
+    if request.method == 'POST':
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listarUsuarios')
+    else:
+        form = PersonaForm()
+    return render(request, "regsitroUsuario.html",{'form':form})
+              
